11b2f12205f81d72204d3960aff4415744347c4c
nicotacca <nicotacca@gmail.com>
2021-06-05 19:03:36 -0300

    commit de prueba

---
+++ Carpeta de prueba/hola mundo.java
@@ -0,0 +1,10 @@
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+
+
+
+class Main {
+    public static void main(String args[]) {
+        System.out.println("Hola BOBOS de mierda 2.0");
+    }
+}
