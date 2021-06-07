07de7ee2b3f949a32a8acdd2b02613295d72adce
nicotacca <nicotacca@gmail.com>
2021-06-07 00:41:40 -0300

    carpeta hola mundo redefinida

---
+++ Carpeta de prueba/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo!");
+        System.out.println("Hola INFO 2021!");
+       
+
+    }
+}
+/* 
+Errores: en este caso el archivo debe llamarse igual que la clase HolaMundo. */
